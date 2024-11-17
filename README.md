730cfffb5db60982d342fbcffbf45d7ad6777a0b
Oswaldo Roberto Marques <oswaldormarques@gmail.com>
2024-11-17 20:05:03 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinamento https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Forever trusting who we are and nothing else matters!</h1>
+  return <h1>Forever trusting who we are and nothing else matters!</h1>;
 }
 
 export default Home;
